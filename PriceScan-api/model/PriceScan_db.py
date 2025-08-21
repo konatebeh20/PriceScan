2bcb3e643752d0ef7b8cc3bc3e1ec1bd1b7c58f7
konatebeh20 <konatebeh20@gmail.com>
2025-08-21 09:08:25 +0000

    PriceScan_db

--- PriceScan-api/model/PriceScan_db.py
+++ PriceScan-api/model/PriceScan_db.py
@@ -13,32 +13,16 @@ from config.db import db # ton instance SQLAlchemy
 
 
 
-class ps_users(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    u_uid = db.Column(db.String(128),unique=True, default=lambda: str(uuid.uuid4()))
-    u_name = db.Column(db.String(128))
-    u_firstname = db.Column(db.String(128))
-    u_lastname = db.Column(db.String(128))
-    u_username = db.Column(db.String(128), unique=True)
-    u_mobile = db.Column(db.String(128))
-    u_address = db.Column(db.String(128))
-    u_country = db.Column(db.String(128))
-    u_state = db.Column(db.String(128))
-    u_city = db.Column(db.String(128))
-    u_email = db.Column(db.String(128))
-    u_image_link = db.Column(db.Text())
-    u_status = db.Column(db.Text(), nullable=False, default=0)
-    u_password = db.Column(db.String(128))
-    u_first_login = db.Column(db.Boolean(),server_default=expression.true(), nullable=False)
-    u_id_scan = db.Column(db.Text())
-    is_active = db.Column(db.Boolean, default=True) 
-    deleted_at = db.Column(db.DateTime, nullable=True)  
-    device_token = db.Column(db.String(128))
-    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
-    updated_on = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
 
-    def as_dict(self):
-       return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+
+
+class UserStatus(enum.Enum):
+    ACTIVE = 1
+    INACTIVE = 0
+    BANNED = -1
+
+
+
 
 class ps_categories(db.Model):
     __tablename__ = "ps_categories"
@@ -55,28 +39,12 @@ class ps_categories(db.Model):
     updated_on = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
 
     # Méthode pour retourner la catégorie en dict (utile pour JSON)
-    def as_dict(self):
-        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+    # def as_dict(self):
+    #     return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
 
+class ps_contact_us(db.Model):
+    __tablename__ = 'ps_contact_us'
 
-
-class go_favorite(db.Model):
-    __tablename__ = 'go_favorite'
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    fav_uid = db.Column(db.String(128), unique=True, default=lambda: str(uuid.uuid4()))
-    u_uid = db.Column(db.String(128), db.ForeignKey('go_users.u_uid'), nullable=False)
-    room_uid = db.Column(db.String(128), db.ForeignKey('go_rooms.room_uid'), nullable=True)
-    htl_uid = db.Column(db.String(128), db.ForeignKey('go_hotels.htl_uid'), nullable=True)
-    status = db.Column(db.String(20), nullable=False)
-    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
-    updated_on = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
-    
-    def as_dict(self):
-        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
-
-
-class go_contact_us(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     ct_uid = db.Column(db.String(128),unique=True, default=lambda: str(uuid.uuid4()))
     ct_name = db.Column(db.String(128))
@@ -88,25 +56,12 @@ class go_contact_us(db.Model):
     creation_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
     updated_on = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
     
-    def as_dict(self):
-       return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+    # def as_dict(self):
+    #    return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
 
+class ps_device_tokens(db.Model):
+    __tablename__ = 'ps_device_tokens'
 
-
-class go_notification(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    notification_id = db.Column(db.String(128), unique=True, default=lambda: str(uuid.uuid4()))
-    header = db.Column(db.String(128))
-    body = db.Column(db.Text())
-    destined_for = db.Column(db.String(128))
-    status = db.Column(db.String(128))
-    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
-
-    def as_dict(self):
-       return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
-
-
-class DeviceTokens(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     device_tokens_id = db.Column(db.String(128), unique=True, default=lambda: str(uuid.uuid4()))
     u_uid = db.Column(db.String(128))
@@ -115,3 +70,68 @@ class DeviceTokens(db.Model):
     is_active = db.Column(db.Boolean, default=True)  # Whether the token is active
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow) # When the token was created
     updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow) # When it was last updated
+
+class ps_users(db.Model):
+    __tablename__ = 'ps_users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    u_uid = db.Column(db.String(128),unique=True, default=lambda: str(uuid.uuid4()))
+    u_name = db.Column(db.String(128))
+    u_firstname = db.Column(db.String(128))
+    u_lastname = db.Column(db.String(128))
+    u_username = db.Column(db.String(128), unique=True)
+    u_mobile = db.Column(db.String(128))
+    u_address = db.Column(db.String(128))
+    u_country = db.Column(db.String(128))
+    u_state = db.Column(db.String(128))
+    u_city = db.Column(db.String(128))
+    u_email = db.Column(db.String(128))
+    u_image_link = db.Column(db.Text())
+    # u_status = db.Column(db.Text(), nullable=False, default=0)
+    u_status = db.Column(db.Enum(UserStatus), nullable=False, default=UserStatus.INACTIVE)
+    u_password = db.Column(db.String(128))
+    u_first_login = db.Column(db.Boolean(),server_default=expression.true(), nullable=False)
+    u_id_scan = db.Column(db.Text())
+    is_active = db.Column(db.Boolean, default=True) 
+    deleted_at = db.Column(db.DateTime, nullable=True)  
+    device_token = db.Column(db.String(128))
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    updated_on = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+    # u_is_verified = db.Column(db.Boolean, default=False)
+    # u_is_active = db.Column(db.Boolean, default=True)
+    # u_is_deleted = db.Column(db.Boolean, default=False)
+    # u_is_banned = db.Column(db.Boolean, default=False)
+    # u_is_active = db.Column(db.Boolean, default=True)
+    # u_is_deleted = db.Column(db.Boolean, default=False)
+    # u_is_banned = db.Column(db.Boolean, default=False)
+
+    # def as_dict(self):
+    #    return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+
+class ps_favorite(db.Model):
+    __tablename__ = 'ps_favorite'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    fav_uid = db.Column(db.String(128), unique=True, default=lambda: str(uuid.uuid4()))
+    u_uid = db.Column(db.String(128), db.ForeignKey('ps_users.u_uid'), nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    updated_on = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    
+    # def as_dict(self):
+    #     return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+
+
+class ps_notification(db.Model):
+    __tablename__ = 'ps_notification'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    notification_id = db.Column(db.String(128), unique=True, default=lambda: str(uuid.uuid4()))
+    header = db.Column(db.String(128))
+    body = db.Column(db.Text())
+    destined_for = db.Column(db.String(128))
+    status = db.Column(db.String(128))
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+
+    # def as_dict(self):
+    #    return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
