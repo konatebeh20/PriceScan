dc0906b3ff2ea5a65fa71776354ae8597f2a6bf3
konatebeh20 <konatebeh20@gmail.com>
2025-08-21 09:08:48 +0000

    PriceScan architecture

--- PriceScan-api/model/PriceScan_db.py
+++ PriceScan-api/model/PriceScan_db.py
@@ -58,8 +58,6 @@ class ps_categories(db.Model):
     def as_dict(self):
         return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
 
-
-
 class go_favorite(db.Model):
     __tablename__ = 'go_favorite'
 
